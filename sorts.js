539536385846f3787b3c1d75f200b3a160a82f39
Taylor Huston <taylor.a.huston@gmail.com>
2015-03-16 00:23:45 -0700

    Implement Quick Sort

--- sorts.js
+++ sorts.js
@@ -8,29 +8,28 @@ var swap = function(toSort, x, y) {
 
 //Entry point
 var masterArray = new Array();
-for (var i = 0; i < 20; i++) {
-    masterArray.push(Math.floor(Math.random()*100) + 1);
+for (var i = 0; i < 200; i++) {
+    masterArray.push(Math.floor(Math.random()*1000) + 1);
 }
 
 console.log(masterArray);
 
 var toSort = masterArray.slice(0);
-console.log(toSort);
 selection(toSort);
 console.log("After selection " + toSort);
 
 toSort = masterArray.slice(0);
-console.log(toSort);
 insertion(toSort);
 console.log("After insertion " + toSort);
 
 toSort = masterArray.slice(0);
-console.log(toSort);
 shell(toSort);
 console.log("After shell " + toSort);
 
-
 toSort = masterArray.slice(0);
-console.log(toSort);
 mergeSort(toSort);
 console.log("After merge  " + toSort);
+
+toSort = masterArray.slice(0);
+quickSort(toSort);
+console.log("After quick  " + toSort);
